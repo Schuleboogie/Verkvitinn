5ab3963606d085630a6d3a4b199ca8d270aa57b2
Sunna Halldórsdóttir <sunna@staki.is>
2016-11-19 13:35:52 +0000

    villusíða komin, ekki komin með útlit

---
+++ src/main/java/project/controller/IndexController.java
@@ -0,0 +1,37 @@
+package project.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import javax.servlet.http.HttpSession;
+import project.service.UserService;
+import project.persistence.entities.User;
+import org.springframework.boot.autoconfigure.web.ErrorController;
+import javax.servlet.http.HttpServletResponse;
+import java.util.regex.Pattern;
+
+@Controller
+public class IndexController implements ErrorController{
+
+    private static final String PATH = "/error";
+
+    @RequestMapping(value = PATH)
+    public String error(Model model, HttpServletResponse response) {
+    	//model.addAttribute("error", response.getStatus());
+    	//System.out.println("heeeeello");
+    	String s= String.valueOf(response.getStatus());
+    	if(s.matches("5..")){
+    		return "errors";
+    	}
+    	else return "redirect:/";
+    }
+
+    @Override
+    public String getErrorPath() {
+        return PATH;
+    }
+}
